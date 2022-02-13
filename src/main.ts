d31faedc6c357fb0b43301a34f7af389f0f94828
Alex-Bruno <bruno-3003@hotmail.com>
2022-02-13 00:19:03 -0300

    :hammer: Main rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
+
+const logger = new Logger('Main');
+const configService = new ConfigService();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const RABBITMQ_USER = configService.get<string>('RABBITMQ_USER');
+  const RABBITMQ_PASSWORD = configService.get<string>('RABBITMQ_PASSWORD');
+  const RABBITMQ_URL = configService.get<string>('RABBITMQ_URL');
+
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@${RABBITMQ_URL}`],
+      queue: 'desafios',
+      noAck: false,
+    },
+  });
+
+  await app.listen();
 }
 bootstrap();
