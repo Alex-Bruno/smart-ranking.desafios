8420f383c23ab465e66510c6befd8f1dc6b6cd07
Alex-Bruno <bruno-3003@hotmail.com>
2022-02-13 00:09:30 -0300

    :hammer: Imports

--- src/partidas/partidas.module.ts
+++ src/partidas/partidas.module.ts
@@ -1,8 +1,17 @@
+import { ProxyrmqModule } from './../proxyrmq/proxyrmq.module';
+import { PartidaSchema } from './interfaces/partida.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { PartidasController } from './partidas.controller';
 import { PartidasService } from './partidas.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {name: 'Partida', schema: PartidaSchema}
+    ]),
+    ProxyrmqModule
+  ],
   controllers: [PartidasController],
   providers: [PartidasService]
 })
